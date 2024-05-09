0d4dd034f5b2d577b8ba4af5ccb7bba58a5820d3
Aleena Shaju <aleenashaju2023@gmail.com>
2024-05-09 20:16:33 +0400

    Add files via upload
    Program to reverse a number (functions with argument and no return type.)

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class ReverseNumber {
+    static void reverseNumber(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        System.out.println("Reversed Number: " + reversedNumber);
+    }
+
+    public static void main(String args[]) {
+        try {
+            DataInputStream dis = new DataInputStream(System.in);
+            System.out.print("Enter a number: ");
+            int inputNumber = Integer.parseInt(dis.readLine());
+            reverseNumber(inputNumber);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
